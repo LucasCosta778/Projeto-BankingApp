3a5aa78bd7fbec2b62dcde40d4e06c0252c188a4
Lucas <lucascta@gmail.com>
2024-02-18 22:45:51 -0300

    Adição de Banco de Dados

--- obj/Debug/net8.0/MinhaApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MinhaApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+845b025649513c094e77162e00f2a71d696844b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
